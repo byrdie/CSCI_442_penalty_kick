a82b320917940d34df225cb7cc6a9d9e3cbcc1bd
zfisher22 <zdfisher1923@yahoo.com>
2015-03-04 11:08:00 -0700

    everything is broken....

--- CSCI442_penalty_kick/main.cpp
+++ CSCI442_penalty_kick/main.cpp
@@ -17,11 +17,13 @@
 #include <alvision/alimage.h>
 #include <alvision/alvisiondefinitions.h>
 #include <alerror/alerror.h>
+#include <alproxies/alledsproxy.h>
 
 // Opencv includes.
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
 
 
 using namespace AL;
@@ -36,6 +38,12 @@ void showImages(const std::string& robotIp) {
     /** Create a proxy to ALVideoDevice on the robot.*/
     ALVideoDeviceProxy camProxy(robotIp, 9559);
 
+    /*Create proxy for controlling LEDs*/
+    AL::ALLedsProxy leds(robotIp, 9559);
+    std::string right = "RightFaceLedsGreen";
+    std::string left = "LeftFaceLedsGreen";
+    std::string both = "FaceLeds";
+
     /** Subscribe a client image requiring 320*240 and BGR colorspace.*/
     const std::string clientName = camProxy.subscribe("test", kQVGA, kBGRColorSpace, 30);
 
@@ -43,6 +51,8 @@ void showImages(const std::string& robotIp) {
     cv::Mat img = cv::Mat(cv::Size(320, 240), CV_8UC3);
     cv::Mat run_ave = cv::Mat(img.size(), CV_8UC3);
     cv::Mat weightedFrame = cv::Mat(img.size(), CV_32FC3);
+    Mat imgLines = Mat::zeros(img.size(), CV_8UC3);
+    cv::Mat hsv = cv::Mat(cv::Size(320, 240), CV_8UC3);
 
     /** Create a OpenCV window to display the images. */
     cv::namedWindow("images", CV_WINDOW_KEEPRATIO);
@@ -50,14 +60,10 @@ void showImages(const std::string& robotIp) {
     //    cv::namedWindow("Averages", CV_WINDOW_KEEPRATIO);
 
     /*create vector of points to store trajectory of ball, only store last 100 points*/
-    vector<Point> com(100); // ball location according to center of mass
-    vector<Point> hough(100); //ball location according to houghs transform
-    vector<Point> traject(100); // final decision of ball location
-
-    /*create image buffers*/
-    cv::Mat hsv = cv::Mat(cv::Size(320, 240), CV_8UC3);
-    Mat imgLines = Mat::zeros(hsv.size(), CV_8UC3);
-
+    unsigned int vec_sz = 20;
+    vector<Point> com(vec_sz); // ball location according to center of mass
+    vector<Point> hough(vec_sz); //ball location according to houghs transform
+    vector<Point> traject(vec_sz); // final decision of ball location
 
     /*HSV values for the YELLOW tennis ball*/;
     int iLowH = 19; // Hue
@@ -79,6 +85,7 @@ void showImages(const std::string& robotIp) {
     cv::createTrackbar("HighV", "Thresholded Image", &iHighV, 255);
 
     unsigned int index = 0; // index to track which frame we're at
+    unsigned int oldi = vec_sz; // index to track which frame we're at
 
     /** Main loop. Exit when pressing ESC.*/
     while ((char) cv::waitKey(30) != 27) {
@@ -98,7 +105,7 @@ void showImages(const std::string& robotIp) {
 
         /*blur frame and take average*/
         cv::GaussianBlur(img, run_ave, Size(5, 5), 0, 0);
-        cv::accumulateWeighted(run_ave, weightedFrame, 0.80);
+        cv::accumulateWeighted(run_ave, weightedFrame, 0.90);
         cv::convertScaleAbs(weightedFrame, run_ave, 1.0, 0.0);
 
         /*convert BGR to HSV*/
@@ -109,8 +116,8 @@ void showImages(const std::string& robotIp) {
         inRange(hsv, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), thresh); //Threshold the image
 
         /*morphological opening, removes small objects from the foreground*/
-        erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
-        dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
+        erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(7, 7)));
+        dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(7, 7)));
 
         /*morphological closing, removes small holes from foreground*/
         dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
@@ -151,24 +158,26 @@ void showImages(const std::string& robotIp) {
         }
 
         /*make sure points are within bounds of the image*/
-        if (com[index].x > 0 && com[index].y > 0 && hough[index].x > 0 && hough[index].y > 0) {
+        if (com[index].x > 0 || com[index].y > 0 || hough[index].x > 0 || hough[index].y > 0) {
 
             /*calculate the distance between point reported by houghs and center of mass*/
             int dX = hough[index].x - com[index].x;
             int dY = hough[index].y - com[index].y;
             double dist_squared = dX * dX + dY * dY;
+            std::cout << "Distance squared is" << dist_squared << std::endl;
 
-            if (dist_squared < 10) { // 10 is the threshold value for how much disagreement there can be between houghs and center of mass
+            if (dist_squared < 30) { // 10 is the threshold value for how much disagreement there can be between houghs and center of mass
 
                 /*If the two are close, set the final trajectory as the average between the two*/
                 traject[index].x = com[index].x + dX;
                 traject[index].y = com[index].y + dY;
 
                 //Draw a red line from the previous point to the current point
-                //                line(imgLines, traject[index], traject[oldi], Scalar(0, 0, 255), 2);
+                line(imgLines, traject[index], traject[oldi], Scalar(255, 0, 0), 2);
 
                 /*increment indices only if we have found a good ball location*/
-                index = (index + 1) % 100;
+                oldi = index;
+                index = (index + 1) % (traject.size() - 1);
 
             } else {
                 std::cout << "Houghs and COM disagreement!" << std::endl;
@@ -179,32 +188,54 @@ void showImages(const std::string& robotIp) {
         }
 
         /*allocate memory for trajectory line*/
-        Mat imgLines = Mat::zeros(img.size(), CV_8UC3);
+        Mat trajectLines = Mat::zeros(img.size(), CV_8UC3);
 
         /*add all points in the traject vector to find average direction*/
-        int sumX;
-        int sumY;
-        Point this_point; // current point in the sum
-        Point last_point; // last point in the sum
-        for (int i = 0; i < traject.size(); i++) {
-            this_point = traject[i];
-            sumX += this_point.x;
-            sumY += this_point.y;
-
-            /*while we're here, we may as well draw all the lines on the image*/
-            if (i != 0) { // add points after we go through the first point
-                //Draw a red line from the previous point to the current point
-                line(imgLines, this_point, last_point, Scalar(0, 0, 255), 2);
+        Point first_point = traject[index];
+        Point last_point = first_point;
+        for (int i = traject.size(); i > 0; i--) {
+            int j = (i + index) % traject.size();
+            if (traject[j].x > 0 && traject[j].y > 0) {
+                last_point = traject[j];
             }
-
-            last_point = this_point; // update last point
         }
 
+        /*draw a line between points*/
+        line(trajectLines, first_point, last_point, Scalar(0, 0, 255), 2);
+
         /*use the average to find the angle theta at which the ball is moving*/
-        double theta = atan(sumY / sumX) * 180 / 3.1415;
+        double theta = 400.0;
+        int difX = first_point.x - last_point.x;
+        int difY = first_point.y - last_point.y;
+        if (difX > 0 && difY > 0) {
+            theta = atan(difY / difX) * 180 / 3.1415;
+        } else {
+            std::cout << "No change" << std::endl;
+        }
+
+
+        /*direction decision*/
+        if (theta < 45.0) { //Right
+            leds.on(right);
+            leds.off(left);
+        } else if (theta >= 45.0 && theta < 135.0) { // towards
+            leds.on(both);
+        } else if (theta >= 135.0 && theta < 270.0) { //left
+            leds.on(left);
+            leds.off(right);
+        } else if (theta >= 270.0 && theta < 360.0) { //right again
+            leds.on(right);
+            leds.off(left);
+        } else {
+            leds.off(both);
+            std::cout << "wtf?" << std::endl;
+        }
 
         /*add lines to original image*/
         img = img + imgLines;
+        img = img + trajectLines;
+
+
 
 
         /** Display the iplImage on screen.*/
@@ -219,7 +250,7 @@ void showImages(const std::string& robotIp) {
 
 int main(int argc, char* argv[]) {
 
-    const std::string robotIp("153.90.197.238");
+    const std::string robotIp("153.90.197.185");
 
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
 
