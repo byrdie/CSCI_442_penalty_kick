2535f51292242bda14c66a10a494cf6f66165cfd
zfisher22 <zdfisher1923@yahoo.com>
2015-03-03 19:13:56 -0700

    first commit

---
+++ sayhelloworld/build-naoqi_sdk/CMakeFiles/sayhelloworld.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/byrdie/zach/CSCI_442_penalty_kick/sayhelloworld/sayhelloworld.cpp" "/home/byrdie/zach/CSCI_442_penalty_kick/sayhelloworld/build-naoqi_sdk/CMakeFiles/sayhelloworld.dir/sayhelloworld.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/byrdie/school/CSCI442_Robot_Vision/naoqi_c++/naoqi-sdk-1.14.5-linux64/include"
+  "/home/byrdie/school/CSCI442_Robot_Vision/naoqi_c++/naoqi-sdk-1.14.5-linux64/include/alsoap"
+  "/home/byrdie/school/CSCI442_Robot_Vision/naoqi_c++/naoqi-sdk-1.14.5-linux64/include/soap"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
