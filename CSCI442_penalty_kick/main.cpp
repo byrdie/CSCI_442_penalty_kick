b09deb0eab33427aa069e83cc6a2e0892b91580a
zfisher22 <zdfisher1923@yahoo.com>
2015-03-04 08:30:24 -0700

    added ball angle calculation

--- CSCI442_penalty_kick/main.cpp
+++ CSCI442_penalty_kick/main.cpp
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
-
+#include <math.h>
 
 // Aldebaran includes.
 #include <alproxies/alvideodeviceproxy.h>
@@ -47,7 +47,7 @@ void showImages(const std::string& robotIp) {
     /** Create a OpenCV window to display the images. */
     cv::namedWindow("images", CV_WINDOW_KEEPRATIO);
     cv::namedWindow("Thresholded Image", CV_WINDOW_KEEPRATIO);
-    cv::namedWindow("Averages", CV_WINDOW_KEEPRATIO);
+    //    cv::namedWindow("Averages", CV_WINDOW_KEEPRATIO);
 
     /*create vector of points to store trajectory of ball, only store last 100 points*/
     vector<Point> com(100); // ball location according to center of mass
@@ -57,7 +57,7 @@ void showImages(const std::string& robotIp) {
     /*create image buffers*/
     cv::Mat hsv = cv::Mat(cv::Size(320, 240), CV_8UC3);
     Mat imgLines = Mat::zeros(hsv.size(), CV_8UC3);
-    ;
+
 
     /*HSV values for the YELLOW tennis ball*/;
     int iLowH = 19; // Hue
@@ -78,20 +78,17 @@ void showImages(const std::string& robotIp) {
     cv::createTrackbar("LowV", "Thresholded Image", &iLowV, 255); //Value (0 - 255)
     cv::createTrackbar("HighV", "Thresholded Image", &iHighV, 255);
 
-    unsigned int i = 0; // index to track which frame we're at
-    unsigned int oldi = 100; // last index
+    unsigned int index = 0; // index to track which frame we're at
 
     /** Main loop. Exit when pressing ESC.*/
     while ((char) cv::waitKey(30) != 27) {
-        /* Retrieve an image from the camera. */
 
+        /* Retrieve an image from the camera. */
         ALValue image = camProxy.getImageRemote(clientName);
 
         /** Access the image buffer (6th field) and assign it to the opencv image
          * container. */
         img.data = (uchar*) image[6].GetBinary();
-
-
 
         /** Tells to ALVideoDevice that it can give back the image buffer to the
          * driver. Optional after a getImageRemote but MANDATORY after a getImageLocal.*/
@@ -125,17 +122,17 @@ void showImages(const std::string& robotIp) {
         HoughCircles(thresh, circles, CV_HOUGH_GRADIENT, 2, thresh.rows, 200, 20, 0, 0);
 
         /*draw circles detected by Hough transform*/
-        for (size_t j = 0; j < circles.size(); j++) {
-            Point center(cvRound(circles[j][0]), cvRound(circles[j][1]));
-            int radius = cvRound(circles[j][2]);
+        for (size_t i = 0; i < circles.size(); i++) {
+            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
             // circle center
             circle(img, center, 3, Scalar(0, 255, 0), -1, 8, 0);
             // circle outline
             circle(img, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 
             /*select the first circle as the thing we want: the ball*/
-            if (j == 0) {
-                hough[i] = center; // set the coordinate in the center as the location of the ball
+            if (i == 0) {
+                hough[index] = center; // set the coordinate in the center as the location of the ball
             }
         }
 
@@ -149,41 +146,62 @@ void showImages(const std::string& robotIp) {
         if (dA > 1000) {
 
             /*calculate position of the ball*/
-            com[i].x = dM10 / dA;
-            com[i].y = dM01 / dA;
+            com[index].x = dM10 / dA;
+            com[index].y = dM01 / dA;
         }
 
         /*make sure points are within bounds of the image*/
-        if (com[oldi].x > 0 && com[oldi].y > 0 && com[i].x > 0 && com[i].y > 0 && hough[oldi].x > 0 && hough[oldi].y > 0 && hough[i].x > 0 && hough[i].y > 0) {
+        if (com[index].x > 0 && com[index].y > 0 && hough[index].x > 0 && hough[index].y > 0) {
 
             /*calculate the distance between point reported by houghs and center of mass*/
-            int dX = hough[i].x - com[i].x;
-            int dY = hough[i].y - com[i].y;
+            int dX = hough[index].x - com[index].x;
+            int dY = hough[index].y - com[index].y;
             double dist_squared = dX * dX + dY * dY;
 
             if (dist_squared < 10) { // 10 is the threshold value for how much disagreement there can be between houghs and center of mass
 
                 /*If the two are close, set the final trajectory as the average between the two*/
-                traject[i].x = com[i].x + dX;
-                traject[i].y = com[i].y + dY;
+                traject[index].x = com[index].x + dX;
+                traject[index].y = com[index].y + dY;
 
                 //Draw a red line from the previous point to the current point
-                line(imgLines, traject[i], traject[oldi], Scalar(0, 0, 255), 2);
+                //                line(imgLines, traject[index], traject[oldi], Scalar(0, 0, 255), 2);
 
-                /*increment indices*/
-                oldi = i;
-                i = (i + 1) % 100;
-                
+                /*increment indices only if we have found a good ball location*/
+                index = (index + 1) % 100;
+
             } else {
                 std::cout << "Houghs and COM disagreement!" << std::endl;
             }
 
-
         } else {
-            std::cout << "Points equal to zero!" << std::endl; 
+            std::cout << "Points equal to zero!" << std::endl;
         }
 
+        /*allocate memory for trajectory line*/
+        Mat imgLines = Mat::zeros(img.size(), CV_8UC3);
 
+        /*add all points in the traject vector to find average direction*/
+        int sumX;
+        int sumY;
+        Point this_point; // current point in the sum
+        Point last_point; // last point in the sum
+        for (int i = 0; i < traject.size(); i++) {
+            this_point = traject[i];
+            sumX += this_point.x;
+            sumY += this_point.y;
+
+            /*while we're here, we may as well draw all the lines on the image*/
+            if (i != 0) { // add points after we go through the first point
+                //Draw a red line from the previous point to the current point
+                line(imgLines, this_point, last_point, Scalar(0, 0, 255), 2);
+            }
+
+            last_point = this_point; // update last point
+        }
+
+        /*use the average to find the angle theta at which the ball is moving*/
+        double theta = atan(sumY / sumX) * 180 / 3.1415;
 
         /*add lines to original image*/
         img = img + imgLines;
@@ -192,7 +210,7 @@ void showImages(const std::string& robotIp) {
         /** Display the iplImage on screen.*/
         cv::imshow("images", img);
         //        cv::imshow("Averages", run_ave);
-        //        imshow("Thresholded Image", thresh); //show the thresholded image
+        imshow("Thresholded Image", thresh); //show the thresholded image
     }
 
     /** Cleanup.*/
