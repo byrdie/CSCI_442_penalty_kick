a627988d705a8e17ff3519ca6c6733d8f292b3c7
zfisher22 <zdfisher1923@yahoo.com>
2015-04-27 19:38:29 -0600

    kicking is horrible

--- CSCI442_penalty_kick/move.cpp
+++ CSCI442_penalty_kick/move.cpp
@@ -57,21 +57,21 @@ void move_to_ball() {
             double head_yaw_rad = head_yaw_deg * 0.017453;
             motion.setAngles("HeadYaw", head_yaw_rad, 0.8);
 
-//            if (!motion.walkIsActive()) {
-                if (head_yaw_deg > 4.0) {
+            //            if (!motion.walkIsActive()) {
+            if (head_yaw_deg > 4.0) {
 
-//                    motion.post.moveTo(0.0, 0.05, 0.0);
-                    motion.setWalkTargetVelocity(0.01, 0.7, 0.0, 1.0);
+                //                    motion.post.moveTo(0.0, 0.05, 0.0);
+                motion.setWalkTargetVelocity(0.01, 0.7, 0.0, 1.0);
 
-                } else if (head_yaw_deg < -4.0) {
-//                    motion.post.moveTo(0.0, -0.05, 0.0);
-                    motion.setWalkTargetVelocity(0.01, -0.7, 0.0, 1.0);
-                } else {
-//                    motion.post.moveTo(0.1, 0.0, 0.0);
-                    motion.setWalkTargetVelocity(1.0, 0.0, 0.0, 1.0);
-                    
-                }
-//            }
+            } else if (head_yaw_deg < -4.0) {
+                //                    motion.post.moveTo(0.0, -0.05, 0.0);
+                motion.setWalkTargetVelocity(0.01, -0.7, 0.0, 1.0);
+            } else {
+                //                    motion.post.moveTo(0.1, 0.0, 0.0);
+                motion.setWalkTargetVelocity(1.0, 0.0, 0.0, 1.0);
+
+            }
+            //            }
 
 
             //            std::cout << "X-coordinate " << ball_loc.x << std::endl;
@@ -82,17 +82,17 @@ void move_to_ball() {
 
 
         } else {
-//            motion.setAngles("HeadYaw", 0.0, 0.8);
-            if(lost_count < 2){
+            //            motion.setAngles("HeadYaw", 0.0, 0.8);
+            if (lost_count < 2) {
                 motion.setWalkTargetVelocity(1.0, 0.0, 0.0, 1.0);
             } else {
                 break;
             }
-            if(!lost){
+            if (lost > 5) {
                 lost_count++;
             }
-            lost = 1;
-            
+            lost++;
+
         }
 
 
@@ -108,14 +108,161 @@ void move_to_ball() {
 
 }
 
-
-void step_around_ball(){
+void step_around_ball() {
     motion.moveTo(-0.02, 0.2, 0);
     motion.moveTo(0.29, 0.05, 0.0);
     motion.moveTo(0.01, -0.26, 0.0);
-    motion.moveTo(-0.5, 0.0, -PI/6);
+    motion.moveTo(-0.5, 0.0, -PI / 6);
 
-    
+
+}
+
+void kick_ball() {
+    AL::ALValue names;
+    AL::ALValue times;
+    AL::ALValue keys;
+
+    names. = "HeadPitch";
+    times = AL::ALValue::array(1.16000, 2.68000, 3.20000, 4.24000, 5.12000, 6.12000);
+    keys = AL::ALValue::array(0.04363, 0.26180, 0.17453, -0.27925, -0.26180, -0.24241);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "HeadYaw";
+    times = AL::ALValue::array(1.16000, 2.68000, 3.20000, 4.24000, 5.12000, 6.12000);
+    keys = AL::ALValue::array(0.00464, -0.00149, 0.00311, -0.04905, -0.03371, -0.02459);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RAnklePitch";
+    times = AL::ALValue::array(1.04000, 1.76000, 2.56000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.03226, 0.01745, 0.01745, 0.03491, 0.03491, 0.11501);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RAnkleRoll";
+    times = AL::ALValue::array(1.04000, 1.76000, 2.56000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(-0.33161, -0.36652, -0.36652, -0.36652, -0.34732, 0.08433);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RElbowRoll";
+    times = AL::ALValue::array(1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000);
+    keys = AL::ALValue::array(0.74096, 1.03396, 1.36990, 1.02015, 0.70722, 0.37732);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RElbowYaw";
+    times = AL::ALValue::array(1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000);
+    keys = AL::ALValue::array(-1.15353, -0.95411, -0.90809, -1.23023, -1.55697, -1.14441);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    //names="RHand"
+    //times=AL::ALValue::array( 1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000)
+    //keys=AL::ALValue::array( 0.00317, 0.00328, 0.00329, 0.00317, 0.00325, 0.00187)
+
+    names = "RHipPitch";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.23159, 0.10580, 0.12217, 0.08433, 0.09046, 0.19171, 0.21020);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RHipRoll";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.34366, 0.36820, 0.36820, 0.36513, 0.36667, 0.36513, -0.10129);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RHipYawPitch";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(-0.18097, -0.25307, -0.06285, -0.05058, -0.18711, -0.24386, -0.31903);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RKneePitch";
+    times = AL::ALValue::array(1.04000, 1.76000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(-0.08727, -0.08727, -0.09235, -0.07973, -0.07973, -0.07819, -0.07666, -0.09208);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RShoulderPitch";
+    times = AL::ALValue::array(1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000);
+    keys = AL::ALValue::array(1.48649, 1.35917, 1.41746, 1.59847, 1.63835, 1.50021);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "RShoulderRoll";
+    times = AL::ALValue::array(1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000);
+    keys = AL::ALValue::array(-0.02305, -0.01998, -0.13197, -0.11816, -0.02305, -0.03524);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    //names="RWristYaw"
+    //times=AL::ALValue::array( 1.08000, 2.60000, 3.12000, 4.16000, 5.04000, 6.04000)
+    //keys=AL::ALValue::array( 0.24435, 0.23935, 0.22094, 0.20253, 0.19026, -0.12736)
+
+    names = "LAnklePitch";
+    times = AL::ALValue::array(1.04000, 1.32000, 1.76000, 2.56000, 2.84000, 3.36000, 3.68000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.08727, -0.08727, -0.59341, 0.12217, -0.05236, 0.24435, -0.12217, -0.64403, -0.21991, 0.11356);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LAnkleRoll";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(-0.40143, -0.10887, -0.13802, -0.00000, -0.18097, -0.34558, -0.05066);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LElbowRoll";
+    times = AL::ALValue::array(1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000);
+    keys = AL::ALValue::array(-0.64117, -1.15353, -1.38056, -1.36062, -0.96024, -0.45564);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LElbowYaw";
+    times = AL::ALValue::array(1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000);
+    keys = AL::ALValue::array(0.99714, 0.86368, 0.90970, 0.63205, 0.84834, 1.49714);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    //names="LHand"
+    //times=AL::ALValue::array( 1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000)
+    //keys=AL::ALValue::array( 0.00129, 0.00136, 0.00132, 0.00128, 0.00133, 0.00391)
+
+    names = "LHipPitch";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.16265, -0.39726, -1.11876, -1.11978, -0.78540, -0.29142, 0.21318);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LHipRoll";
+    times = AL::ALValue::array(1.04000, 2.56000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(0.47124, 0.54001, 0.32218, 0.12276, 0.36360, 0.41713, 0.05825);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LKneePitch";
+    times = AL::ALValue::array(1.04000, 2.56000, 2.84000, 3.08000, 3.36000, 4.12000, 5.00000, 6.00000);
+    keys = AL::ALValue::array(-0.08901, 1.97575, 1.97222, 1.23918, 0.24435, 1.53589, 0.62430, -0.07666);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LShoulderPitch";
+    times = AL::ALValue::array(1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000);
+    keys = AL::ALValue::array(1.52782, 1.46033, 1.47413, 1.24096, 1.51862, 1.54938);
+    motion.angleInterpolation(names, keys, times, 1);
+
+
+    names = "LShoulderRoll";
+    times = AL::ALValue::array(1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000);
+    keys = AL::ALValue::array(0.12268, 0.04138, 0.14569, 0.13955, 0.14722, 0.03993);
+    //names.push_back("LWristYaw")
+    //times.push_back([ 1.00000, 2.52000, 3.04000, 4.08000, 4.96000, 5.96000])
+    //keys.push_back([ -0.08727, -0.07359, -0.05058, -0.06285, 0.05680, 0.00149])
+
+    motion.angleInterpolation(names, keys, times, 1);
+
 }
 
 /*normalize and flip components, since x is forward in robot's frame*/
